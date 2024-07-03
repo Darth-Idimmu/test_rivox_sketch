1f7dc6c0dc2fb9bf2a83880c573795a97a851c12
Darth_Idimmu_pc <dafquin@hotmail.com>
2024-07-03 13:39:13 -0500

    comps_app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import LiveWagers from './components/LiveWagers';
+import OpenWagers from './components/OpenWagers';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <LiveWagers />
+      <OpenWagers />
     </div>
   );
 };
