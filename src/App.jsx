0074fd1ed8fc815585848c20b8f9854f375d31bb
Darth_Idimmu_pc <dafquin@hotmail.com>
2024-07-03 18:36:15 -0500

    app_actualice

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header';// Nuevo componente
 import LiveWagers from './components/LiveWagers';
-import OngoingWagers from './components/OngoingWagers';
 import OpenWagers from './components/OpenWagers';
+import OngoingWagers from './components/OngoingWagers';
+import Footer from './components/Footer'; // Nuevo componente
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      <LiveWagers />
-      <OpenWagers />
-      <OngoingWagers />
+      <main className="main-content">
+        <LiveWagers />
+        <OpenWagers />
+        <OngoingWagers />
+      </main>
+      <Footer />
     </div>
   );
 };
