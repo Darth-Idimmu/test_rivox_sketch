6d785e1fff1e2c45dd7719f48d0d500a9e90897f
Darth_Idimmu_pc <dafquin@hotmail.com>
2024-07-03 12:13:39 -0500

    hearder_prefinish

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FaHome, FaGamepad, FaTrophy, FaShoppingBag } from "react-icons/fa";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav>
+      <header className="header">
+        <div className="header-left">
+          <div className="logo">Rivox</div>
+          <button className="menu-button">⋮</button>
+        </div>
+        <div className="header-center">
+          <div className="icon-container">
+            <FaHome className="icon" title="Home" />
+          </div>
+          <div className="icon-container">
+            <FaGamepad className="icon" title="Games" />
+          </div>
+          <div className="icon-container">
+            <FaTrophy className="icon" title="Trophies" />
+          </div>
+          <div className="icon-container">
+            <FaShoppingBag className="icon" title="Shop" />
+          </div>
+        </div>
+        <div className="header-right">
+          <button className="auth-button">Login</button>
+          <button className="auth-button" style={{background: '#713bf3'}}>Register</button>
+          <button className="more-options-button">⋮</button>
+        </div>
+      </header>
+    </nav>
+  );
+};
+
+export default Header;
