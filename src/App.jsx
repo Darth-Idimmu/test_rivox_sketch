684dcddba96b578c7c9a51c556c86b0f37ec372d
Darth_Idimmu_pc <dafquin@hotmail.com>
2024-07-03 19:44:09 -0500

    actualice_app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import LiveWagers from './components/LiveWagers';
 import OpenWagers from './components/OpenWagers';
 import OngoingWagers from './components/OngoingWagers';
 import Footer from './components/Footer';
+import LiveWagersInfo from './components/LiveWagersInfo'
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <main className="main-content">
+        <LiveWagersInfo />
         <LiveWagers />
         <OpenWagers />
         <OngoingWagers />
